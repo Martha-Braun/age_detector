7ba1a0f21296ca083725a2df03209c97a93c60ee
Martha <martha.e.braun@gmail.com>
2021-12-15 17:01:56 +0100

    add face detector

--- streamlit_dash/age_tool.py
+++ streamlit_dash/age_tool.py
@@ -5,6 +5,7 @@ import streamlit as st
 import datetime
 from PIL import Image
 import cv2
+import matplotlib as plt
 
 
 class DashboardApp:
@@ -36,7 +37,8 @@ class DashboardApp:
         option = st.sidebar.selectbox(
             "Pick Dashboard:",
             (
-                "Main Page",
+                "Take Picture",
+                "Detect Faces",
                 "Party Time!",
             ),
         )
@@ -50,7 +52,7 @@ class DashboardApp:
         """
 
         # Option Main Page
-        if self.option == "Main Page":
+        if self.option == "Take Picture":
             st.title("Step 1: Take a Picture")
             # st.markdown("Take a picture and we'll guess youre age")
             st.subheader("Smile!")
@@ -64,11 +66,43 @@ class DashboardApp:
                 _, frame = camera.read()  # return a single frame in variable `frame`
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 FRAME_WINDOW.image(frame)
-                cv2.imwrite("streamlit_dash/c1.png", frame)
+                cv2.imwrite("streamlit_dash/pictures/picture_taken/c1.png", frame)
             else:
-                st.write("Stopped")
+                st.write("Click Run to Start Webcam")
 
-            st.title("Step 2: Predict Age")
+        # Option Predict Age
+        if self.option == "Detect Faces":
+            st.title("Step 2: Detect Faces")
+
+            folder_path = "streamlit_dash/pictures/picture_taken"
+            image_file = "c1.png"
+            # take the image and convert it to an OpenCV object
+            image = cv2.imread(os.path.join(folder_path, image_file))
+            # convert it to grayscale
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            faceCascade = cv2.CascadeClassifier(
+                cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+            )
+            faces = faceCascade.detectMultiScale(
+                gray, scaleFactor=1.3, minNeighbors=3, minSize=(30, 30)
+            )
+
+            detected_image = image.copy()
+            for idx, (x, y, w, h) in enumerate(faces):
+                cv2.rectangle(detected_image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                roi_color = image[y : y + h, x : x + w]
+                print("[INFO] Object found. Saving locally.")
+                cv2.imwrite(
+                    "pictures/faces_detected/face_" + str(idx) + "_" + image_file,
+                    roi_color,
+                )
+
+            status = cv2.imwrite(
+                "pictures/faces_detected/faces_detected.jpg", detected_image
+            )
+
+            st.image(detected_image)
 
         # Option Welcome
         if self.option == "Party Time!":
